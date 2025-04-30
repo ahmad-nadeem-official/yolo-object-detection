7aee6e7faaa0701663961747694d7ed36fcf5ca2
ahmad-nadeem-official <ahmadnadeem701065@gmail.com>
2025-04-30 16:22:49 +0500

    yolo

--- YOLO/src/function.py
+++ YOLO/src/function.py
@@ -2,8 +2,22 @@
 main function to run the YOLO model
 '''
 
+'''install ultralytics if not installed'''
+# !pip install ultralytics
+
+from ultralytics import YOLO
+
 def detection(path, confidence, model):
   print("welcome to obejct detection by YOLO")
   model = YOLO(model)
   result = model(source=path, show=True, conf=confidence, save=True)
   return result
+
+path = input("please enter the path of video :")
+confidence = float(input("please enter the confidence value: "))
+model = input("please enter the model :")
+if model == "":
+  model = "yolov8n.pt"
+
+
+detection(path, confidence, model)
